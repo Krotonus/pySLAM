b8aaffb3344c62fdaf1676c4f011f326d6d853d2
Krotonus <lalithm.work@gmail.com>
2020-08-31 09:34:18 +0530

    Imports g2o without errors. Changed the feature extraction class.

---
+++ frame.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+from skimage.measure import ransac 
+from skimage.transform import EssentialMatrixTransform
+from skimage.transform import FundamentalMatrixTransform
+
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
+IRt = np.eye(4)
+
+def extractRt(E):
+    W = np.mat([[0, -1, 0],
+                [1, 0, 0 ],
+                [0, 0, 1 ]], dtype = float)
+    U, d, Vt = np.linalg.svd(E)
+
+    assert np.linalg.det(U) > 0
+
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0
+    R = np.dot(np.dot(U, W), Vt)
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), Vt)
+    t = U[:, 2]
+    ret = np.eye(4)
+    ret[:3, :3] = R
+    ret[:3, 3] = t
+    return ret
+
+def normalize(Kinv, pts):
+    return np.dot(Kinv, add_ones(pts).T).T[:, 0:2]
+
+def denormalize(K, pt):
+    ret = np.dot(K, np.array([pt[0], pt[1], 1.0]))
+    ret /= ret[2]
+    return int(round(ret[0])), int(round(ret[1]))
+
+def extract(img):
+    orb = cv2.ORB_create()
+    pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8),
+            maxCorners = 3000,
+            qualityLevel = 0.01,
+            minDistance = 3)
+
+    kps = [cv2.KeyPoint(f[0][0], f[0][1], _size = 20) for f in pts]
+    kps, des = orb.compute(img, kps)
+
+    return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
+
+def match_frames(f1, f2):
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    matches = bf.knnMatch(f1.des, f2.des, k = 2)
+
+    ret = []
+    idx1, idx2 = [], []
+
+    for m, n in matches:
+        if m.distance < 0.75*n.distance:
+            idx1.append(m.queryIdx)
+            idx2.append(m.trainIdx)
+
+            p1 = f1.pts[m.queryIdx]
+            p2 = f2.pts[m.trainIdx]
+            ret.append((p1, p2))
+
+    assert len(ret) >= 8
+
+    ret = np.array(ret)
+    idx1 = np.array(idx1)
+    idx2 = np.array(idx2)
+
+    model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                            EssentialMatrixTransform,
+                            #FundamentalMatrixTransform,
+                            min_samples = 8,
+                            residual_threshold = 0.5,
+                            max_trials = 100)
+
+    print(sum(inliers), len(inliers))
+    Rt = extractRt(model.params)
+
+    return idx1[inliers], idx2[inliers], Rt
+
+class Frame(object):
+    def __init__(self, img, K):
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+        self.pose = IRt
+
+        pts, self.des = extract(img)
+        self.pts = normalize(self.Kinv, pts)
--- slam.py
+++ slam.py
@@ -1,39 +1,73 @@
+import os
+os.environ['PATH'] = "./g2o" + os.pathsep + os.environ['PATH']
+
+import g2o
+import time
 import numpy as np
 import cv2
 from display import Display
-from feature_extractor import FeatureExtractor
+from frame import Frame, denormalize, match_frames, IRt
 
 W = 1920//2
 H = 1080//2
-F = 1
+F = 270 
 K = np.array([[F, 0, W//2],
               [0, F, H//2],
               [0, 0, 1]])
 
 screen = Display(W, H)
-extractor = FeatureExtractor(K)
 
+class Point(object):
+    def __init__(self, loc):
+        self.location = loc
+        self.frames = []
+        self.idxs = []
 
+    def add_observation(self, frame, idx):
+        self.frames.append(frame)
+        self.idxs.append(idx)
+
+def triangulate(pose1, pose2, pts1, pts2):
+    return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+
+frames = []
 def process_frame(img):
     img = cv2.resize(img, (W, H))
-    matches, pose = extractor.extract(img)
-    if pose is None:
+    frame = Frame(img, K)
+    frames.append(frame)
+    if len(frames) <= 1:
         return
 
-    print(f'{len(matches)} matches')
-    print(pose)
+    idx1, idx2, Rt = match_frames(frames[-1], frames[-2])
+    frames[-1].pose = np.dot(Rt, frames[-2].pose)
 
-    for p1, p2 in matches:
-        u1, v1 = extractor.denormalize(p1)
-        u2, v2 = extractor.denormalize(p2)
+    pts4d = triangulate(frames[-1].pose, frames[-2].pose, frames[-1].pts[idx1], frames[-2].pts[idx2])
+    pts4d /= pts4d[:, 3:]
+
+    good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
+
+    #print(f'{len(matches)} matches')
+
+    for i, p in enumerate(good_pts4d):
+        if not good_pts4d[i]:
+            continue
+        pt = Point(p)
+        pt.add_observation(frames[-1], idx1[i])
+        pt.add_observation(frames[-2], idx2[i])
+
+    for p1, p2 in zip(frames[-1].pts[idx1], frames[-2].pts[idx2]):
+        u1, v1 = denormalize(K, p1)
+        u2, v2 = denormalize(K, p2)
         cv2.circle(img, (u1, v1), radius = 3, color = (0, 255, 0))
         cv2.line(img, (u1, v1), (u2, v2), color = (255, 0, 0))
-
     screen.paint(img)
     
 if __name__ == "__main__":
     cap = cv2.VideoCapture("./videos/test.mp4")
-    while(cap.isOpened()):
-        ret, image = cap.read()
-        process_frame(image)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            process_frame(frame)
+        else:
+            break
     print("Done.")
